1ace3452e01c824b3fb8b999ff1d224bd1ca5cd1
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-01-31 08:34:00 +0000

    Allow All dmain cors setting.

--- index.js
+++ index.js
@@ -39,7 +39,14 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  cors: {
+    origin: '*',
+    credentials: true
+  }
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
